6843485763993e6bae2fe0d957bca5a4a91ba141
oscarfnp <oscar.fnunezp@gmail.com>
2015-07-07 12:32:23 -0500

    removing trailing/leading spaces from data

--- medicare.gov/official_supplier_directory/bundle.py
+++ medicare.gov/official_supplier_directory/bundle.py
@@ -8,10 +8,13 @@ class Bundle(CsvBundle):
     @staticmethod
     def int_na_caster(v):
         try:
-            return int(v)
+            return int(v.strip())
         except ValueError:
             return None
-
-#    def line_mangler(self, source, l):
-
-#        return l.replace('\0', '')
+    
+    def build_modify_row(self, row_gen, p, source, row):
+        """Several of the data elements have trailing spaces, 
+        this removes them
+        """
+        for i in row.keys():
+            row[i] = row[i].strip()
